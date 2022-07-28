d923ea65ead00e8dd5b44cc52ad1003521850e4a
utbrott <rosinskimk@gmail.com>
2022-07-28 15:58:12 +0200

    Fundamentals: Loops

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ A repository archive for Go (Golang) course
 - [x] Variables
 - [x] Functions
 - [x] Conditionals / Switches
+- [x] Loops 
+
+### Data structures & types
+- [ ] Structures 
+- [ ] Arrays 
+- [ ] Slices 
+- [ ] Maps 
+- [ ] Pointers 
--- zerotomastery.io-golang/src/lectures/exercise/loops/loops.go
+++ zerotomastery.io-golang/src/lectures/exercise/loops/loops.go
@@ -15,4 +15,23 @@ package main
 import "fmt"
 
 func main() {
+	//* Print integers 1 to 50, except:
+	//  - Print "Fizz" if the integer is divisible by 3
+	//  - Print "Buzz" if the integer is divisible by 5
+	//  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
+	for i := 1; i <= 50; i++ {
+		var toPrint string
+
+		if i%3 == 0 {
+			toPrint += "Fizz"
+		}
+		if i%5 == 0 {
+			toPrint += "Buzz"
+		}
+		if i%3 != 0 && i%5 != 0 {
+			toPrint = fmt.Sprintf("%v", i)
+		}
+
+		fmt.Println(toPrint)
+	}
 }
