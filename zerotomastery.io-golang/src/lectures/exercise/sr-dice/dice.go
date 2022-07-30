50ac0482dd14132aa146e3a25c063d5d9a7a8f3b
utbrott <rosinskimk@gmail.com>
2022-07-30 16:21:00 +0200

    Section review: Fundamentals

--- zerotomastery.io-golang/src/lectures/exercise/sr-dice/dice.go
+++ zerotomastery.io-golang/src/lectures/exercise/sr-dice/dice.go
@@ -16,7 +16,46 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func roll(sides int) int {
+	return rand.Intn(sides) + 1
+}
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	dice, sides := 2, 6
+	rolls := 2
+
+	for r := 1; r <= rolls; r++ {
+		sum := 0
+
+		for d := 1; d <= dice; d++ {
+			rolled := roll(sides)
+			sum += rolled
+			fmt.Println("Roll #", r, "Die #", d, ":", rolled)
+		}
+		fmt.Println("Total rolled:", sum)
+
+		switch sum := sum; {
+		//  - "Snake eyes": when the total roll is 2, and total dice is 2
+		case sum == 2 && dice == 2:
+			fmt.Println("Snake eyes!")
+		//  - "Lucky 7": when the total roll is 7
+		case sum == 7:
+			fmt.Println("Lucky 7!")
+		//  - "Even": when the total roll is even
+		case sum%2 == 0:
+			fmt.Println("Even!")
+		//  - "Odd": when the total roll is odd
+		case sum%2 != 0:
+			fmt.Println("Odd!")
+		}
+	}
+
 }
